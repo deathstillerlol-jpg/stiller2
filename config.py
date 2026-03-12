ed5ec44db569bff2fdbf359448aed51471cc187c
deathstillerlol-jpg <death.stiller.lol@gmail.com>
2026-03-12 22:35:37 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+BOT_TOKEN = "8757500911:AAEbSh9hlRam0GYC1HdkoXCGTd9Q1vVBeNc"
+API_ID = 31462757          # с my.telegram.org
+API_HASH = "79ae4e151e84526e11b107e99ad67177"
+OWNER_ID = 8559221549      # твой Telegram ID (узнай через @userinfobot)
